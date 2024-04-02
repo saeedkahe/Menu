5f25567f668821b5eecc658061a18fef8b3f477c
Saeed Kahe <155805760+saeedkahe@users.noreply.github.com>
2024-04-02 22:50:36 +0330

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const hamburger_menu = document.querySelector('.hamburger-menu');
+const container = document.querySelector('.container');
+
+hamburger_menu.addEventListener('click', function(){
+    container.classList.toggle('active');
+})
+
